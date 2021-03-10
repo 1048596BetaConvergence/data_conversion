7fa132c4ffceed8bf127a97fd8acdb17c7b5e871
unknown <huyunhao@gmail.com>
2021-03-09 21:38:58 -0500

    commit test cases

--- cli.py
+++ cli.py
@@ -5,10 +5,10 @@ import json
 def iterate_json_file(json_file):
     result = []
     for key, value in json_file.items():
-        if key == 'data':
+        if key == "data":
             result = result + iterate_class_object(value)
         else:
-            raise Exception('json object must start with key "data"')
+            sys.sterr.write("json object must start with key \"data\"")
     return result
 
 def iterate_class_object(class_array):
@@ -20,16 +20,16 @@ def iterate_class_object(class_array):
 
 def merge_class(class_name, object_array):
     for item in object_array:
-        item['class'] = class_name
+        item["class"] = class_name
     return object_array
 
 def iterate_findings(finding_object):
     result = []
     for key, value in finding_object.items():
-        if key == 'findings':
+        if key == "findings":
             result = iterate_finding_object(value)        
         else:
-            raise Exception('class object must start with key "findings"')
+            sys.sterr.write("class object must start with key \"findings\"")
     return result
 
 def iterate_finding_object(finding_array):
@@ -37,68 +37,68 @@ def iterate_finding_object(finding_array):
     for item in finding_array:
         result_object = {}
         for key, value in item.items():
-            if key == 'startLineNumber':
+            if key == "startLineNumber":
                 result_object[key] = value
-            elif key == 'endLineNumber':
+            elif key == "endLineNumber":
                 result_object[key] = value
-            elif key == 'location':
-                path = value.rsplit('/', 1)
+            elif key == "location":
+                path = value.rsplit("/", 1)
                 if len(path) == 2:
-                    result_object['repository'] = path[0]
-                    result_object['file'] = path[1]
+                    result_object["repository"] = path[0]
+                    result_object["file"] = path[1]
                 else:
-                    result_object['repository'] = ''
-                    result_object['file'] = path[0]
-            elif key == 'type':
+                    result_object["repository"] = ""
+                    result_object["file"] = path[0]
+            elif key == "type":
                 result_object[key] = value
             else:
-                print('unrecognized key: ', key)
+                print("unrecognized key: ", key)
                 sys.exit()
         result_array.append(result_object)
     return result_array
 
 
-if __name__ == '__main__':
-    _source_url = ''
-    _repo = ''
-    _class = ''
-    _type = ''
+if __name__ == "__main__":
+    _source_url = ""
+    _repo = ""
+    _class = ""
+    _type = ""
 
     for command in sys.argv:
-        single_command = command.split('=')
+        single_command = command.split("=")
         
-        if command == 'cli.py':
+        if command == "cli.py":
             continue
         elif len(single_command) > 2:
-            raise Exception('Unrecognized command format: ', command)
-        elif single_command[0] == '--sourceUrl':
+            sys.sterr.write("Unrecognized command format: ", command)
+        elif single_command[0] == "--sourceUrl":
             if(len(_source_url)) > 0:
-                raise Exception('Duplicate command --sourceUrl')
+                sys.sterr.write("Duplicate command --sourceUrl")
             _source_url = single_command[1]
-        elif single_command[0] == '--repo':
+        elif single_command[0] == "--repo":
             if(len(_repo)) > 0:
-                raise Exception('Duplicate command --repo')
+                sys.sterr.write("Duplicate command --repo")
             _repo = single_command[1]
-        elif single_command[0] == '--class':
+        elif single_command[0] == "--class":
             if(len(_class)) > 0:
-                raise Exception('Duplicate command --class')
+                sys.sterr.write("Duplicate command --class")
             _class = single_command[1]
-        elif single_command[0] == '--type':
+        elif single_command[0] == "--type":
             if(len(_type)) > 0:
-                raise Exception('Duplicate command --type')
+                sys.sterr.write("Duplicate command --type")
             _type = single_command[1]
         else:
-            raise Exception("Unrecognized command: ", command)
+            sys.sterr.write("Unrecognized command: ", command)
     
     if len(_source_url) == 0:
-        raise Exception("sourceUrl not found from command")
+        sys.sterr.write("sourceUrl not found from command")
 
     with urllib.request.urlopen(_source_url) as _source_file:
         result = iterate_json_file(json.load(_source_file))
         if len(_repo) > 0:
-            result = list(filter(lambda x: ('github.com/' + _repo in x['repository']), result))
+            result = list(filter(lambda x: (_repo in x["repository"]), result))
         if len(_class) > 0:
-            result = list(filter(lambda x: (_class == x['class']), result))
+            result = list(filter(lambda x: (_class == x["class"]), result))
         if len(_type) > 0:
-            result = list(filter(lambda x: (_type == x['type']), result))
-        return result
+            result = list(filter(lambda x: (_type == x["type"]), result))
+        sys.stdout.write(result)
--- test.py
+++ test.py
@@ -1,4 +1,38 @@
 import os
+import urllib.request
+import json
+import subprocess
+import sys
+
+def run(cmd):
+    proc = subprocess.Popen([sys.executable, cmd],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    stdout, stderr = proc.communicate()
+    return stdout, stderr
 
 def base_case_test():
-    os.system('py cli.py --sourceUrl=')
+    output, err = run("py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json")
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+        assert json.load(result)["result_base"] == output
+
+def repo_case_test():
+    output, err = run("py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --repo=github.com/myorg/c")
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+        assert json.load(result)["result_repo"] == output
+
+def class_case_test():
+    output, err = run("py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --class=injection")
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+        assert json.load(result)["result_class"] == output
+
+def type_case_test():
+    output, err = run("py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --type=weak-crypto-2")
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+        assert json.load(result)["result_type"] == output
+
+def command_fail_test():
+    output, err = run("py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --sourceUrl=https://raw.github.com --type=weak-crypto-2")
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+        assert json.load(result)["result_error"] == output
