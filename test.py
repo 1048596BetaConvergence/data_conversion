90e43fbca8a45f8586bef77489b41289d32ab1a7
unknown <huyunhao@gmail.com>
2021-03-09 22:27:59 -0500

    change master to main

--- test.py
+++ test.py
@@ -13,31 +13,31 @@ def run(cmd):
     return stdout, stderr
 
 def test_base_case():
-    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json".split()
+    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample.json".split()
     output, err = run(input_dir)
-    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample_result.json") as result:
         assert json.dumps(json.load(result)["result_base"]) == output.decode("UTF-8").replace("'", "\"").replace("\n", "").replace("\r", "")
 
 def test_repo_case():
-    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --repo=github.com/myorg/c".split()
+    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample.json --repo=github.com/myorg/c".split()
     output, err = run(input_dir)
-    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample_result.json") as result:
         assert json.dumps(json.load(result)["result_repo"]) == output.decode("UTF-8").replace("'", "\"").replace("\n", "").replace("\r", "")
 
 def test_class_case():
-    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --class=injection".split()
+    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample.json --class=injection".split()
     output, err = run(input_dir)
-    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample_result.json") as result:
         assert json.dumps(json.load(result)["result_class"]) == output.decode("UTF-8").replace("'", "\"").replace("\n", "").replace("\r", "")
 
 def test_type_case():
-    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --type=weak-crypto-2".split()
+    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample.json --type=weak-crypto-2".split()
     output, err = run(input_dir)
-    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample_result.json") as result:
         assert json.dumps(json.load(result)["result_type"]) == output.decode("UTF-8").replace("'", "\"").replace("\n", "").replace("\r", "")
 
 def test_command_fail():
-    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample.json --type=weak-crypto-2".split()
+    input_dir = "cli.py --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample.json --sourceUrl=https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample.json --type=weak-crypto-2".split()
     output, err = run(input_dir)
-    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/master/test/sample_result.json") as result:
+    with urllib.request.urlopen("https://raw.githubusercontent.com/1048596BetaConvergence/data_conversion/main/test/sample_result.json") as result:
         assert json.load(result)["result_error"] == err.decode("UTF-8")
